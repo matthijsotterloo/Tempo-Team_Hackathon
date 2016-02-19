0f16386e527cb8496f3be3f021c41b93fb8d08f6
Tom de ruiter <tom@rydee.nl>
2016-02-19 19:50:43 +0100

    Hallo

--- HackathonTempoTeam/ViewController.swift
+++ HackathonTempoTeam/ViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
