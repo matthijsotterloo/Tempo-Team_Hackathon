a5d94ace45e4636b67de99143fbf96387ce883f8
Tom de ruiter <tom@rydee.nl>
2016-02-19 20:25:20 +0100

    Edit PodFile

---
+++ HackathonTempoTeam/BEXML.swift
@@ -0,0 +1,13 @@
+//
+//  BEXML.swift
+//  HackathonTempoTeam
+//
+//  Created by Tom de ruiter on 19/02/16.
+//  Copyright © 2016 Rydee. All rights reserved.
+//
+
+import UIKit
+
+class BEXML: NSObject {
+
+}
--- HackathonTempoTeam/ViewController.swift
+++ HackathonTempoTeam/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // user + pass fields
+    
     @IBOutlet var userTextField: UITextField!
+    @IBOutlet var passTextField: UITextField!
+    
+    // login button
+    
+    @IBOutlet var loginButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        loginButton.layer.cornerRadius = 14
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
